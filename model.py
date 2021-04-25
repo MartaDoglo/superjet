42f24930e6cb30bf13694dac3b64e346e21d49c5
Efimova Anastasia <martatrnastya@gmail.com>
2021-04-25 16:05:13 +0300

    add train.ipynb

--- model.py
+++ model.py
@@ -1,18 +1,3 @@
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-from PIL import Image, ImageDraw
-import os
-
-
-def changed_pic(filename):
-    while True:
-        file = 'uploads/' + filename
-        if os.path.isfile(file):
-            source_img = Image.open(file)
-            figure, ax = plt.subplots()
-
-            rect = patches.Rectangle((5, 5), 100, 100, linewidth=1, edgecolor='r', facecolor='none')
-            ax.imshow(source_img)
-            ax.add_patch(rect)
-            plt.savefig('pictures/' + filename)
-            break
+def changed_pic(file):
+    # predict_function(file) from train.ipynb
+    pass
